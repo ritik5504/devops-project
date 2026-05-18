8d60403e5f9d5c68a98be13a58ca576c06d45e19
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2026-05-18 23:17:43 +0530

    new changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello Ritik, DevOps Project Running Successfully 🚀</h1>');
+    res.send('<h1>CI/CD Auto Deploy Working 🚀</h1>');
 });
 
 app.listen(3000, () => {
